f49135e2e7a12ebbfce4aae77818d625d8d744fa
Murphy Randle <murphyrandle@gmail.com>
2016-05-04 21:12:48 -0600

    Checkpoint 1

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,38 @@
 module Main (..) where
 
 import Html exposing (..)
+import StartApp.Simple as StartApp exposing (start)
+import Signal exposing (Address)
+import EasyEvents exposing (onInput)
+import Markdown
 
-main = div [] [text "Hello there!"]
+type Action
+  = NoOp
+  | UpdateMarkdown String
+
+type alias Model = String
+
+model : Model
+model = ""
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    NoOp -> model
+    UpdateMarkdown str -> str
+
+view : Address Action -> Model -> Html
+view address model =
+  div []
+    [ textarea [onInput address UpdateMarkdown] []
+    , h1 [] [ text "Markdown:" ]
+    , Markdown.toHtml model
+    , h1 [] [ text "Model:" ]
+    , text (toString model) ]
+
+main : Signal Html
+main = start
+  { model = model
+  , update = update
+  , view = view
+  }
